bdeda5a63203e9ed082971b3790e156601ec1207
Yassine <ge87won@mytum.de>
2023-08-18 17:22:33 +0200

    Pushed with git fast

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,25 @@
 // src/components/Header.js
 
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../images/logo.png';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header>
       <Link to="/">
         <img src={logo} alt="Logo" />
       </Link>
-      <nav>
+      <button className="menu-toggle" onClick={toggleMenu}>
+        ☰
+      </button>
+      <nav className={isMenuOpen ? 'active' : ''}>
         <ul>
           <li><Link to="/ethereum-index">Ethereum index</Link></li>
           <li><Link to="/arbitrum-index">Arbitrum index</Link></li>
@@ -18,7 +27,7 @@ const Header = () => {
           <li>
             <Link to="/index-products" className="special-button">
               Index products 
-              <span className="external-link-icon">🔗</span> {/* Here's the emoji as an external link icon */}
+              <span className="external-link-icon">🔗</span>
             </Link>
           </li>
         </ul>
