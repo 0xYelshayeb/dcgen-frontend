ab5911e6f17b8ac285c4c7a23ad080dc8aa50702
Yassine <ge87won@mytum.de>
2023-08-18 17:32:53 +0200

    Pushed with git fast

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,24 +16,25 @@ const Header = () => {
       <Link to="/">
         <img src={logo} alt="Logo" />
       </Link>
-      <button className="menu-toggle" onClick={toggleMenu}>
-        ☰
-      </button>
       <nav className={isMenuOpen ? 'active' : ''}>
         <ul>
-          <li><Link to="/ethereum-index">Ethereum index</Link></li>
-          <li><Link to="/arbitrum-index">Arbitrum index</Link></li>
-          <li><Link to="/research">Research</Link></li>
+          <li><Link to="/ethereum-index" onClick={toggleMenu}>Ethereum index</Link></li>
+          <li><Link to="/arbitrum-index" onClick={toggleMenu}>Arbitrum index</Link></li>
+          <li><Link to="/research" onClick={toggleMenu}>Research</Link></li>
           <li>
-            <Link to="/index-products" className="special-button">
+            <Link to="/index-products" className="special-button" onClick={toggleMenu}>
               Index products 
               <span className="external-link-icon">🔗</span>
             </Link>
           </li>
         </ul>
       </nav>
+      <button className="menu-toggle" onClick={toggleMenu}>
+        {isMenuOpen ? 'x' : '☰'}
+      </button>
     </header>
   );
 };
+
 
 export default Header;
