7c746194bd1069d576759effe7220d39db402d35
Yassine <ge87won@mytum.de>
2023-08-31 00:10:43 +0200

    Added indexname

---
+++ src/components/IndexName.js
@@ -0,0 +1,15 @@
+// src/components/IndexName.js
+
+import React from 'react';
+import molecule from '../images/index-molecule.png';
+
+const IndexName = () => {
+  return (
+    <div className='index-name'>
+      <img className ='index-image' src={molecule} alt="Logo" />
+    </div>
+  )
+};
+
+
+export default IndexName;
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -4,11 +4,13 @@ import Header from "../components/Header";
 import ChartSection from "../components/ChartSection";
 import Information from "../components/Information"
 import Footer from '../components/Footer';
+import IndexName from "../components/IndexName";
 
 const Index = () => {
     return (
         <div className="app">
           <Header />
+          <IndexName />
           <ChartSection />
           <Information />
           <Footer />
