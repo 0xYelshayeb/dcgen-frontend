a30e561784307bf147be68338084479f2b1d606c
Yassine <ge87won@mytum.de>
2023-09-02 16:56:18 +0200

    Adjusted footer for smaller screens

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ import telegram from "../icons/telegram.svg"
 const Footer = () => {
     return (
         <footer className="footer-container">
-            <div className="footer-section">
+            <div className="footer-section footer-section-logo">
                 <div className="footer-logo">
                     <Link to="/">
                         <img src={logo} alt="DCgen Logo" />
@@ -26,17 +26,19 @@ const Footer = () => {
                     <a href="#link5"><img src={gitbook} alt="GitBook Icon" /></a>
                 </div>
             </div>
-            <div className="footer-section">
-                <h4>Community</h4>
-                <a href="https://www.facebook.com/YourPage/">Twitter </a>
-                <a href="https://www.twitter.com/YourPage/">Telegram </a>
-                <a href="https://www.instagram.com/YourPage/">Discord </a>
-            </div>
-            <div className="footer-section">
-                <h4>Resources</h4>
-                <a href="https://www.facebook.com/YourPage/">Documentation </a>
-                <a href="https://www.twitter.com/YourPage/">GitHub </a>
-                <Link to="/research">Research</Link>
+            <div className="footer-sections-wrapper">
+                <div className="footer-section footer-section-rest">
+                    <h4>Community</h4>
+                    <a href="https://www.facebook.com/YourPage/">Twitter </a>
+                    <a href="https://www.twitter.com/YourPage/">Telegram </a>
+                    <a href="https://www.instagram.com/YourPage/">Discord </a>
+                </div>
+                <div className="footer-section footer-section-rest">
+                    <h4>Resources</h4>
+                    <a href="https://www.facebook.com/YourPage/">Documentation </a>
+                    <a href="https://www.twitter.com/YourPage/">GitHub </a>
+                    <Link to="/research">Research</Link>
+                </div>
             </div>
         </footer>
     );
