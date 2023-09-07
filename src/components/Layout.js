bb9bf72080b853b01f35f8bba533c515afc0e2f9
Yassine <ge87won@mytum.de>
2023-09-07 16:17:32 +0200

    Changed more to DCgen

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,13 +11,13 @@ const Layout = ({ children, title, name, description, schema }) => {
         <title>{title}</title>
         <meta name={name} content={description} />
         {schema && <script type="application/ld+json">{JSON.stringify(schema)}</script>}
-        <meta property="og:title" content="DCGen" />
+        <meta property="og:title" content="DCgen" />
         <meta property="og:description" content="Ethereum Ecosystem Index" />
         <meta property="og:image" content="https://dcgen.netlify.app/dcgen.jpeg" />
         <meta property="og:url" content="https://dcgen.netlify.app" />
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="DCGen" />
+        <meta name="twitter:title" content="DCgen" />
         <meta name="twitter:description" content="Ethereum Ecosystem Index" />
         <meta name="twitter:image" content="https://dcgen.netlify.app/dcgen.jpeg" />
       </Helmet>
