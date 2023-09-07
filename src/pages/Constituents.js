f26297ab34e0fc5710c42a0bd1ae2e4df6544f11
Yassine <ge87won@mytum.de>
2023-09-07 23:10:37 +0200

    Made constituents dynamic

--- src/pages/Constituents.js
+++ src/pages/Constituents.js
@@ -1,37 +1,34 @@
 // src/pages/Constituents.js
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import Layout from '../components/Layout';
 import '../styles/constituents.css';
 import IndexName from '../components/IndexName';
 
 const Constituents = () => {
+
+  const [data, setData] = useState([]);  // Added state to store data
+
+  useEffect(() => {
+    const fetchData = async () => {
+        try {
+          const response = await axios.get(`http://165.232.113.176/constituents`);
+          setData(response.data);  // Set state with fetched data
+        } catch (error) {
+          console.error("Failed to fetch data", error);
+        }
+    };
+      
+    fetchData();
+  }, []);
+
   const schema = {
     "@context": "https://dcgen.finance/ethereum-constituents",
     "@type": "WebPage",
     "name": "Ethereum Index Constituents",
     "description": "A Website showing all the constituents of the DCGen Ethereum Ecosystem Index",
   };
-  const data = [
-    { name: 'Bitcoin', symbol: 'BTC', price: '$42,000', marketCap: "$5,000,000", indexPercent: "30 %"},
-    { name: 'Ethereum', symbol: 'ETH', price: '$2,800', marketCap: "$200,000", indexPercent: "70 %"},
-    { name: 'Bitcoin', symbol: 'BTC', price: '$42,000', marketCap: "$5,000,000", indexPercent: "30 %"},
-    { name: 'Ethereum', symbol: 'ETH', price: '$2,800', marketCap: "$200,000", indexPercent: "70 %"},
-    { name: 'Bitcoin', symbol: 'BTC', price: '$42,000', marketCap: "$5,000,000", indexPercent: "30 %"},
-    { name: 'Ethereum', symbol: 'ETH', price: '$2,800', marketCap: "$200,000", indexPercent: "70 %"},
-    { name: 'Bitcoin', symbol: 'BTC', price: '$42,000', marketCap: "$5,000,000", indexPercent: "30 %"},
-    { name: 'Ethereum', symbol: 'ETH', price: '$2,800', marketCap: "$200,000", indexPercent: "70 %"},
-    { name: 'Bitcoin', symbol: 'BTC', price: '$42,000', marketCap: "$5,000,000", indexPercent: "30 %"},
-    { name: 'Ethereum', symbol: 'ETH', price: '$2,800', marketCap: "$200,000", indexPercent: "70 %"},
-    { name: 'Bitcoin', symbol: 'BTC', price: '$42,000', marketCap: "$5,000,000", indexPercent: "30 %"},
-    { name: 'Ethereum', symbol: 'ETH', price: '$2,800', marketCap: "$200,000", indexPercent: "70 %"},
-    { name: 'Bitcoin', symbol: 'BTC', price: '$42,000', marketCap: "$5,000,000", indexPercent: "30 %"},
-    { name: 'Ethereum', symbol: 'ETH', price: '$2,800', marketCap: "$200,000", indexPercent: "70 %"},
-    { name: 'Bitcoin', symbol: 'BTC', price: '$42,000', marketCap: "$5,000,000", indexPercent: "30 %"},
-    { name: 'Ethereum', symbol: 'ETH', price: '$2,800', marketCap: "$200,000", indexPercent: "70 %"},
-
-    // Add more data here...
-  ];
 
   return (
     <Layout title="Eth index Constituents" name="Ethereum ecosystem Index Constituents" description="This page shows the constituents of the ethereum ecosystem index" schema={schema}>
@@ -57,11 +54,11 @@ const Constituents = () => {
           <tbody>
             {data.map((item, index) => (
               <tr key={index}>
-                <td>{item.name}</td>
-                <td class="hide-on-extra-small">{item.symbol}</td>
-                <td class="hide-on-small">{item.price}</td>
-                <td class="hide-on-medium">{item.marketCap}</td>
-                <td>{item.indexPercent}</td>
+                <td>{item.Name}</td>
+                <td class="hide-on-extra-small">{item.Symbol}</td>
+                <td class="hide-on-small">${parseFloat(item.Price).toLocaleString('en-US', { minimumFractionDigits: 4, maximumFractionDigits: 4 })}</td>
+                <td class="hide-on-medium">${parseFloat(item["Market cap"]).toLocaleString('en-US')}</td>
+                <td>{parseFloat(item["Allocation %"]).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })} %</td>
               </tr>
             ))}
           </tbody>
