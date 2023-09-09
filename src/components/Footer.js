52803f8c4e0b87747cc2a12c31a13571d6d078f6
Yassine <ge87won@mytum.de>
2023-09-09 18:01:13 +0200

    Pushed with git fast

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,6 @@ import x from "../icons/x.svg"
 import discord from "../icons/discord.svg"
 import gitbook from "../icons/gitbook.svg"
 import github from "../icons/github.svg"
-import telegram from "../icons/telegram.svg"
 
 const Footer = () => {
     return (
@@ -21,7 +20,6 @@ const Footer = () => {
                 <div className="footer-icons">
                     <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/CodeForFinance"><img src={x} alt="X Icon" /></a>
                     <a target="_blank" rel="noopener noreferrer" href="#link2"><img src={discord} alt="Discord Icon" /></a>
-                    <a target="_blank" rel="noopener noreferrer" href="#link3"><img src={telegram} alt="Telegram Icon" /></a>
                     <a target="_blank" rel="noopener noreferrer" href="#link4"><img src={github} alt="GitHub Icon" /></a>
                     <a target="_blank" rel="noopener noreferrer" href="https://dcgen.gitbook.io/dcgen/"><img src={gitbook} alt="GitBook Icon" /></a>
                 </div>
@@ -30,7 +28,6 @@ const Footer = () => {
                 <div className="footer-section footer-section-rest">
                     <h4>Community</h4>
                     <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/CodeForFinance">X (Twitter) </a>
-                    <a target="_blank" rel="noopener noreferrer" href="#Telegram">Telegram </a>
                     <a target="_blank" rel="noopener noreferrer" href="#Discord">Discord </a>
                 </div>
                 <div className="footer-section footer-section-rest">
