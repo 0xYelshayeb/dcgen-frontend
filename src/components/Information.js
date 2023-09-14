4cb865489840d1629005ecb9d48f40ddf3def9de
Yassine <ge87won@mytum.de>
2023-09-14 15:20:27 +0200

    made constituetns in the front page dymanic

--- src/components/Information.js
+++ src/components/Information.js
@@ -1,9 +1,8 @@
 // src/components/Information.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import uniswap from "../images/uniswap.png"
-import aave from "../images/aave.png"
-import makerdao from "../images/makerdao.png"
 import preview1 from "../images/preview1.jpg"
 import preview2 from "../images/preview2.jpg"
 
@@ -15,7 +14,7 @@ const Information = () => {
         const fetchData = async () => {
             try {
                 const response = await axios.get(`https://api.dcgen.finance/constituents`);
-                setConstituents(response.data);  // Set state with fetched data
+                setConstituents(response.data.slice(0, 3));  // Fetch only first 3 objects
             } catch (error) {
                 console.error("Failed to fetch data", error);
             }
@@ -57,27 +56,18 @@ const Information = () => {
                     <p>Curated with DCgen's precision-driven classification algorithm and upholding rigorous eligibility criteria, these 30 tokens are the hallmark of value for Ethereum enthusiasts, encapsulating the very essence of every DCgen index.</p>
                 </div>
                 <Link to="/ethereum-constituents" id="tokens-container">
-                    <div className="token">
-                        <div className="left-group">
-                            <img src={uniswap} alt="icon1" />
-                            <span className="token-name">Uniswap</span>
+                    {constituents.map((item, index) => (
+                        <div className="token" key={index}>
+                            <div className="left-group">
+                                {/* You can dynamically set the image source if you have a mapping */}
+                                <img src={uniswap} alt={`icon${index + 1}`} />
+                                <span className="token-name">{item.Name}</span>
+                            </div>
+                            <span className="token-change">
+                                {parseFloat(item["Allocation %"]).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })} %
+                            </span>
                         </div>
-                        <span className="token-change">13.45%</span>
-                    </div>
-                    <div className="token">
-                        <div className="left-group">
-                            <img src={aave} alt="icon2" />
-                            <span className="token-name">Aave</span>
-                        </div>
-                        <span className="token-change">11.25%</span>
-                    </div>
-                    <div className="token">
-                        <div className="left-group">
-                            <img src={makerdao} alt="icon3" />
-                            <span className="token-name">MakerDAO</span>
-                        </div>
-                        <span className="token-change">8.72%</span>
-                    </div>
+                    ))}
                     <div className="redirect-button-container">
                         <div>
                             <i className="fas fa-angle-right" style={{ color: 'black' }}></i>
