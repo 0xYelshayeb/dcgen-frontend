0204921f1094882daf123b4f7cf71cd4acf86aed
Yassine <ge87won@mytum.de>
2024-03-03 21:38:04 +0100

    Pushed with git fast

--- src/pages/Research.js
+++ src/pages/Research.js
@@ -1,58 +1,104 @@
 // src/pages/Research.js
-import React from "react";
-import Layout from "../components/Layout";
-import preview1 from "../images/preview1-hd.png"
-import preview2 from "../images/preview2-hd.png"
-import "../styles/research.css"
+import React from 'react';
+import {
+  Box,
+  VStack,
+  Heading,
+  Text,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Divider,
+  SimpleGrid,
+  Image,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import Layout from '../components/Layout';
+
+// Assuming you have these images in your project's public directory or imported
+// import marketChartImg from '../images/market-chart.svg';
 
 const Research = () => {
+  const papers = [
+    {
+      topic: 'Index Methodology',
+      title: 'An empirical approach and practical framework for a decentralized Ethereum Ecosystem Index (EEI)',
+      description: 'This research introduces a new index for the Ethereum ecosystem, analyzing the performance differences between capitalization-weighted and equal-weighted index strategies.',
+    },
+    {
+      topic: 'Index Optimization',
+      title: 'Analyzing the Quantitative Impact of Constituent Counts and Rebalancing Intervals on Ethereum Market Index Performance Metrics.',
+      description: 'This study investigates the effects of varying the number of constituents and rebalancing intervals on the performance of Ethereum market indices.',
+    },
+  ];
 
-    const papers = [
-        {
-          title: "Ethereum Ecosystem Index: A Comprehensive Study of Index Construction and Performance.",
-          description: "In the multifaceted cryptocurrency ecosystem, the need for accurate benchmarks has never been more pressing. This study introduces an index for the Ethereum ecosystem, comparing capitalization-weighted (CW) and equal-weighted (EW) methodologies. Utilizing data from January 2021 to April 2023, the research reveals CW indices outperform EW by an average total return of 127.63%. The proposed CW30 index surpasses existing, widely adopted benchmarks. This research paper serves as a foundational cornerstone for DCgen Index Research, shaping our approach and index methodologies. It also provides any market participants with a robust, data-backed framework for understanding and engaging with the Ethereum ecosystem.",
-          img: preview1,
-          pdf: '/DCgen_Research1.pdf'
-        },
-        {
-          title: "Analyzing the Quantitative Impact of Constituent Counts and Rebalancing Intervals on Ethereum Market Index Performance Metrics.",
-          description: "This study dives deep into the Capitalization-Weighted (CW) Ethereum Market Index to reveal the quantitative significance of varying constituent counts and rebalancing intervals. Using data from January 2020 to July 2023, the paper concludes that an index with 30 constituents and an 8-week rebalancing interval exhibits superior performance across multiple metrics, including Total Return, Mean Return, and key risk-adjusted ratios like Sharpe and Sortino. This specific configuration accounts for an astounding 73.6% variation in total returns, serving as a milestone for those aiming for balanced and optimal portfolio performance. This research serves as a foundation for DCgen Index Research.",
-          img: preview2,
-          pdf: '/DCgen_Research2.pdf'
-        }
-      ];
+  const borderColor = useColorModeValue('gray.200', 'gray.600');
 
-    const schema = {
-        "@context": "https://dcgen.finance/research",
-        "@type": "WebPage",
-        "name": "DCGen Research Page",
-        "description": "Page containing all the Research for the Ethereum Ecosystem Index",
-      };
-      return (
-        <Layout title="Research" name="Research Page" description="Page including research papers released by dcgen" schema={schema}>
-        <div className="main">
-          <section className="hero-section">
-            <h1>Research</h1>
-            <p>Delve into DCgen's research, where in-depth analysis meets the complexities of crypto index construction and performance. Browse our collection of research papers, each shedding light on the nuances of index metrics, composition, and the Ethereum ecosystem's dynamics.</p>
-          </section>
-          <hr style={{borderColor: "#D0D1D3"}} />
-          {papers.map((paper, index) => (
-            <section key={index} className="paper-section">
-              <div className="description">
-                <h2>{paper.title}</h2>
-                <p>{paper.description}</p>
-              </div>
-              <div className="document-preview">
-                <img src={paper.img} alt={paper.title} />
-                <a href={paper.pdf} download>
-                    <button>Download</button>
-                </a>
-              </div>
-            </section>
-          ))}
-        </div>
-        </Layout>
-      );
+  return (
+    <Layout>
+      <VStack pt={15} pb={15} gap={15}>
+        <VStack p={10} align="left" width="70%" gap={5}>
+          <Box>
+            <Heading size="lg" mb={4}>Research & Insights</Heading>
+            <Text>
+              Dive into the heart of crypto insights with our thorough analyses. Discover our research papers that unravel the intricacies of index construction, product performance, and the fast-changing world of the Ethereum ecosystem.
+            </Text>
+          </Box>
+
+          <Table variant="simple" colorScheme="transparent" size="md">
+            <Thead bgColor={borderColor}>
+              <Tr>
+                <Th borderColor={borderColor}>Topic</Th>
+                <Th borderColor={borderColor}>Title</Th>
+                <Th borderColor={borderColor}>Summary</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {papers.map((paper, index) => (
+                <Tr key={index}>
+                  <Td borderColor={borderColor}>{paper.topic}</Td>
+                  <Td borderColor={borderColor}>{paper.title}</Td>
+                  <Td borderColor={borderColor}>{paper.description}</Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+
+        </VStack>
+
+        <Divider orientation="horizontal" />
+
+        <VStack p={10} align="left" width="70%" gap={5}>
+          <Box>
+            <Heading size="lg" mb={4}>Market Benchmarks</Heading>
+            <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
+              <Image src="" alt="Market Benchmark Chart" />
+              <VStack align="start">
+                <Text fontWeight="bold">Summary</Text>
+                <Text>The Ethereum Governance Index by DCgen is a market-capitalization weighted benchmark tracking the top thirty governance tokens on Ethereum, offering a dynamic measure of this segment's performance.</Text>
+                <Box>
+                  <Text fontWeight="bold">Index Level</Text>
+                  <Text>$ 142.56</Text>
+                </Box>
+                <Box>
+                  <Text fontWeight="bold">1M Change</Text>
+                  <Text color="green.500">32.63%</Text>
+                </Box>
+                <Text fontWeight="bold">Weighting Method</Text>
+                <Text>capitalization-weighted</Text>
+                {/* <Link href="#" isExternal>
+                More details <ExternalLinkIcon mx="2px" />
+              </Link> */}
+              </VStack>
+            </SimpleGrid>
+          </Box>
+        </VStack>
+      </VStack>
+    </Layout >
+  );
 };
 
 export default Research;
