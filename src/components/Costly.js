8f73696cf296406af040b34f3075eef7619313b0
Yassine <ge87won@mytum.de>
2024-03-07 21:02:04 +0100

    Added accentuation with bold tags

--- src/components/Costly.js
+++ src/components/Costly.js
@@ -95,7 +95,7 @@ const Costly = () => {
                 <MotionImage variants={item} initial="hidden" animate={controls} src={ChartSVG} alt="Typical Fees for Financial Products" />
                 <MotionVStack align="left" spacing={4} variants={item} initial="hidden" animate={controls}>
                     <Text fontSize="lg" fontWeight="bold" color={colors.t2Blue}>The Silent Profit Eater</Text>
-                    <Text>Hidden streaming fees stealthily chip away at potential profits, culminating in significant underperformance over time.</Text>
+                    <Text>Hidden<Text as="span" fontWeight="bold" > streaming fees</Text> stealthily chip away at potential profits, culminating in <Text as="span" fontWeight="bold" >significant underperformance</Text> over time.</Text>
                     <Table variant="simple" size="md" bg="transparent">
                         <Thead bgColor={borderColor}>
                             <Tr bg="transparent">
