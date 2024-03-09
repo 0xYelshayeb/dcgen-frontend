041bd2ece1ebd8e94fbe494860c7a73daf5055d4
Yassine <ge87won@mytum.de>
2024-03-09 17:38:25 +0100

    Added disclamer

--- src/components/product-overview/InfoSection.js
+++ src/components/product-overview/InfoSection.js
@@ -116,6 +116,41 @@ const InfoSection = () => {
                     </ModalBody>
                 </ModalContent>
             </Modal>
+            <Flex direction='column'>
+                <Text fontSize={'2xs'}>
+                    Disclaimer: This content is for informational purposes only and is not
+                    legal, tax, investment, financial, or other advice. You should not take,
+                    or refrain from taking, any action based on any information contained
+                    herein, or any other information that we make available at any time,
+                    including blog posts, data, articles, links to third-party content,
+                    discord content, news feeds, tutorials, tweets, and videos. Before you
+                    make any financial, legal, technical, or other decisions, you should seek
+                    independent professional advice from a licensed and qualified individual
+                    in the area for which such advice would be appropriate. This information
+                    is not intended to be comprehensive or address all aspects of Index or its
+                    products. There is additional documentation on Index's website about the
+                    functioning of Index Coop, and its ecosystem and community.
+                    <br />
+                    <br />
+                    You shall not purchase or otherwise acquire our restricted tokens if you
+                    are: a citizen, resident (tax or otherwise), green card holder,
+                    incorporated in, owned or controlled by a person or entity in, located in,
+                    or have a registered office or principal place of business in the U.S. (a
+                    “U.S. Person”), or if you are a person in any jurisdiction in which such
+                    offer, sale, and/or purchase of any of our token products is unlawful,
+                    prohibited, or unauthorized (together with U.S. Person, a “Restricted
+                    Person”). The term “Restricted Person” includes, but is not limited to,
+                    any natural person residing in, or any firm, company, partnership, trust,
+                    corporation, entity, government, state or agency of a state, or any other
+                    incorporated or unincorporated body or association, association or
+                    partnership (whether or not having separate legal personality) that is
+                    established and/or lawfully existing under the laws of, a jurisdiction in
+                    which such offer, sale, and/or purchase of any of our token products is
+                    unlawful, prohibited, or unauthorized). You shall not resell or otherwise
+                    transfer our restricted tokens to any Restricted Person. The transfer or
+                    resale of our restricted tokens to any Restricted Person is not permitted.
+                </Text>
+            </Flex>
         </VStack>
     );
 };
